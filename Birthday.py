b1e5c99b42c58303a8d4e0775b95013cc8150659
PranshiSelvam <pranshiselvam11@gmail.com>
2024-10-05 13:08:50 +0530

    Birthdays

--- Birthday.py
+++ Birthday.py
@@ -4,4 +4,7 @@ Third_birthday = "10 - 08 - 1975"
 Fourth_birthday = "10 - 05 - 2015"
 Fifth_birthday = "28 - 12 - 2021"
 print("Pranshi's birthday is on" , First_birthday)
-print("Moms ")
+print("Moms birthday is on" , Second_birthday)
+print("Dads birthday is on" , Third_birthday)
+print("Pennys birthday is on" , Fourth_birthday)
+print("Waffles birthday is on" , Fifth_birthday)
